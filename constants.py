fce4c0ae239b070191289589b7c9edc8b15c49f7
Ubuntu <ubuntu@ip-172-31-77-108.ec2.internal>
2021-06-03 02:13:32 +0000

    added in  pizza_cost.py

---
+++ constants.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+# Created by: Mr. Coxall
+# Created on: Sep 2020
+# This file holds the constants for cost_of_pizza 
+
+LABOR = 0.75 
+RENT = 1.00
+COST_PER_INCH = 0.50
+HST = 0.13
